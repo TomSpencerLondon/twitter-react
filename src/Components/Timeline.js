9dbf9fc4c04dd97511487c4d7d04480b43b0c87f
Tom <tom@homeflow.co.uk>
2020-01-18 15:44:02 +0000

    Add fetch request to local API for Timeline component

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Styles/App.css';
 import Tweet from './Tweet';
 
 function Timeline() {
+  useEffect(() => getInfo());
+  const [items, setItems] = useState([]);
+  const getInfo = () => {
+    fetch('http://localhost:8080', { method: 'GET' })
+      .then((data) => data.json())
+      .then((data) => setItems(data))
+  }
   return(
     <div className="App">
       <Tweet />
